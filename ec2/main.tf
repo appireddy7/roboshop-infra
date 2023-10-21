e8002f744857e68238f659013c45dc4a9400c460
Appi Reddy <appireddy.777@outlook.com>
2023-10-21 21:24:23 +0530

    abv-modules

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "devops-practice-with-ansible"
+  name_regex       = "dops-praevctice-with-ansible"
   owners           = [data.aws_caller_identity.current.account_id]
 }
 
